d961ba3843cfc1f2b90636373ae0796b9e52d454
Eric Danan <ricouilletaff@gmail.com>
2020-10-08 22:10:59 +0100

    Remove obsolete code from cb_options.js

--- mail_extension/cb_options.js
+++ mail_extension/cb_options.js
@@ -23,33 +23,6 @@ function store_settings() {
     })
 }
 
-function storeSettings() {
-  browser.storage.local.set({
-    copyID: {
-      prefix: prefixInput.value,
-      suffix: suffixInput.value,
-      copyBrackets: copyBrackets.checked,
-      urlEncode: urlEncode.checked,
-      raw: raw.checked
-    }
-  });
-}
-
-/*
-Update the options UI with the settings values retrieved from storage,
-or the default settings if the stored settings are empty.
-*/
-function updateUI(storedSettings) {
-  if (storedSettings.copyID) {
-    prefixInput.value = storedSettings.copyID.prefix;
-    suffixInput.value = storedSettings.copyID.suffix;
-    copyBracketsInput.checked = storedSettings.copyID.copyBrackets;
-    urlEncodeInput.checked = storedSettings.copyID.urlEncode;
-    rawInput.checked = storedSettings.copyID.raw;
-  }
-}
-
-
 /*
 On opening the options page, fetch stored settings and update the UI with them.
 */
@@ -66,9 +39,9 @@ browser.storage.local.get().then((settings) => {
 /*
 On checkbox change, save the currently selected settings.
 */
-copyBracketsInput.addEventListener("change", storeSettings);
-urlEncodeInput.addEventListener("change", storeSettings);
-rawInput.addEventListener("change", storeSettings);
+copyBracketsInput.addEventListener("change", store_settings);
+urlEncodeInput.addEventListener("change", store_settings);
+rawInput.addEventListener("change", store_settings);
 
 document.querySelector("#three_pane")   .addEventListener('change', store_settings)
 document.querySelector("#new_tab")      .addEventListener('change', store_settings)
